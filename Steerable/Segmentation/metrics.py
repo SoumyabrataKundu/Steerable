d194ec0aa2951203316d16b88f3d10e95d7a7a43
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-05-07 10:30:02 -0500

    Update

--- Steerable/Segmentation/metrics.py
+++ Steerable/Segmentation/metrics.py
@@ -1,14 +1,18 @@
 import torch
 
 class Metrics:
-    def __init__(self, num_classes):
-        self.num_classes = num_classes
-        self.confusion = torch.zeros(self.num_classes, self.num_classes, dtype=torch.int64)
+    def __init__(self, num_classes, metric='dice'):
+        if isinstance(num_classes) is not int or num_classes<0:
+            raise ValueError(f"num_classes {num_classes} must be an integer greated than 0.")
+        if metric not in ['dice', 'iou', 'accuracy']:
+            raise ValueError(f"metric ({metric}) must be one of 'dice', 'iou' or 'accuracy'.")
         
-    #####################################################################################################
-    ####################################### Confusion Matrix ############################################
-    #####################################################################################################
-
+        self.num_classes = num_classes
+        self.metric = metric
+        self.confusion = torch.zeros(1, self.num_classes, self.num_classes, dtype=torch.int64)
+        
+        
+    ################## Confusion Matrix
     def get_confusion_matrix(self, preds, targets):
         if preds is None and targets is None:
             return self.confusion
@@ -25,75 +29,45 @@ class Metrics:
             raise ValueError('Either both preds and targets should be given or none of them.')
         
     def add_to_confusion_matrix(self, preds, targets):
-        self.confusion += self.get_confusion_matrix(preds, targets).sum(dim=0)
+        self.confusion += torch.sum(self.get_confusion_matrix(preds, targets), dim=0, keepdim=True)
 
-    #####################################################################################################
-    ####################################### Dice Score ##################################################
-    #####################################################################################################
-    
-    def dice_per_class(self, preds=None, targets=None):
+    ################## Metrics
+    def macro_per_class(self, preds=None, targets=None):
         conf = self.get_confusion_matrix(preds, targets)
         tp = torch.diagonal(conf, dim1=-2, dim2=-1)
         fp = torch.sum(conf, dim=-1) - tp
         fn = torch.sum(conf, dim=-2) - tp
-
-        dice = torch.nan_to_num(2*tp / (2*tp + fp + fn), 1)
-        return dice if preds is None else dice.mean(dim=0)
-
-    
-    def mDice(self, preds=None, targets=None):
-        return torch.mean(self.dice_per_class(preds, targets)[1:]).item()
-    
-    def MDice(self):
-        conf = self.confusion
-        tp = torch.diagonal(conf, dim1=-2, dim2=-1)[1:].sum()
-        fp = (torch.sum(conf, dim=-1) - tp)[1:].sum()
-        fn = (torch.sum(conf, dim=-2) - tp)[1:].sum()
         
-        return torch.nan_to_num(2*tp / (2*tp + fp + fn), 1).item()
-        
-    #####################################################################################################
-    ################################################ IOU ################################################
-    #####################################################################################################
+        return torch.nan_to_num(self._get_score(tp,fp,fn), 1).mean(dim=0)
 
-    def iou_per_class(self, preds=None, targets=None):
+    def macro(self, preds=None, targets=None):
+        return torch.mean(self.macro_per_class(preds, targets)[1:]).item()
+    
+    def micro_per_class(self, preds=None, targets=None):
         conf = self.get_confusion_matrix(preds, targets)
-        tp = torch.diagonal(conf, dim1=-2, dim2=-1)
-        fp = torch.sum(conf, dim=-1) - tp
-        fn = torch.sum(conf, dim=-2) - tp
-
-        iou = torch.nan_to_num(tp / (tp + fp + fn), 1)
-        return iou if preds is None else iou.mean(dim=0)
+        tp = torch.diagonal(conf, dim1=-2, dim2=-1).sum(dim=0)
+        fp = (torch.sum(conf, dim=-1) - tp).sum(dim=0)
+        fn = (torch.sum(conf, dim=-2) - tp).sum(dim=0)
+            
+        return torch.nan_to_num(self._get_score(tp,fp,fn), 1)
     
-    def mIOU(self, preds=None, targets=None):
-        return torch.mean(self.iou_per_class(preds, targets)[1:]).item()
-    
-    def MIOU(self):
-        conf = self.confusion
-        tp = torch.diagonal(conf, dim1=-2, dim2=-1)[1:].sum()
-        fp = (torch.sum(conf, dim=-1) - tp)[1:].sum()
-        fn = (torch.sum(conf, dim=-2) - tp)[1:].sum()
-        
-        return torch.nan_to_num(tp / (tp + fp + fn), 1).item()
-
-    #####################################################################################################
-    ################################################ Accuracy ###########################################
-    #####################################################################################################
-
-    def acc_per_class(self, preds=None, targets=None):
+    def micro(self, preds=None, targets=None):
         conf = self.get_confusion_matrix(preds, targets)
-        tp = torch.diagonal(conf, dim1=-2, dim2=-1)
-        fp = torch.sum(conf, dim=-1) - tp
-
-        iou = torch.nan_to_num(tp / (tp + fp), 1)
-        return iou if preds is None else iou.mean(dim=0)
-
-    def mAcc(self, preds=None, targets=None):
-        return torch.mean(self.acc_per_class(preds, targets)[1:]).item()
+        tp = torch.diagonal(conf, dim1=-2, dim2=-1)[:,1:].sum()
+        fp = (torch.sum(conf, dim=-1) - tp)[:,1:].sum()
+        fn = (torch.sum(conf, dim=-2) - tp)[:,1:].sum()
+            
+        return torch.nan_to_num(self._get_score(tp,fp,fn), 1).item()
 
 
-    def MAcc(self):
-        conf = self.confusion
-        tp = torch.diagonal(conf, dim1=-2, dim2=-1).sum()
+    def _get_score(self, tp, fp, fn):
+        if self.metric == 'dice':
+            score = 2*tp / (2*tp + fp + fn)
 
-        return torch.nan_to_num(tp / conf.sum(), 1).item()
+        elif self.metric == 'iou':
+            score = tp / (tp + fp + fn)
+        
+        elif self.metric == 'accuracy':
+            score = tp / (tp + fp)
+            
+        return score

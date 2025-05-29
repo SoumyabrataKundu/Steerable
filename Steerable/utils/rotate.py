1d87715bbcadc6c6285cf26d249e81bac1317c98
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-05-29 10:36:45 -0500

    Update

--- Steerable/utils/rotate.py
+++ Steerable/utils/rotate.py
@@ -6,8 +6,10 @@ class RotateImage:
         self.order = order
     
     def rotate_image_slice(self, image_slice, degree):
-        image_slice = torch.from_numpy(rotate(image_slice, degree[0], (1,0), reshape=False, order=self.order))
+        if image_slice.ndim == 2:
+            image_slice = torch.from_numpy(rotate(image_slice, degree, (1,0), reshape=False, order=self.order))
         if image_slice.ndim == 3:
+            image_slice = torch.from_numpy(rotate(image_slice, degree[0], (1,0), reshape=False, order=self.order))
             image_slice = torch.from_numpy(rotate(image_slice, degree[1], (0,2), reshape=False, order=self.order))
             image_slice = torch.from_numpy(rotate(image_slice, degree[2], (1,0), reshape=False, order=self.order))
         return image_slice
@@ -15,7 +17,7 @@ class RotateImage:
         
     def __call__(self, image, degree=None):
         assert image.ndim in [3,4], f'Only accepted 3D (C,H,W) or 4D (C,H,W,D) tensors but got {image.ndim}D tensor.'
-        degree = degree if degree else torch.randint(0, 360, (3,))
+        degree = torch.tensor(degree) if degree else torch.randint(0, 360, (3,))
             
         return torch.vstack([self.rotate_image_slice(image[i], degree) for i in range(image.shape[0])]).view(*image.shape)
             

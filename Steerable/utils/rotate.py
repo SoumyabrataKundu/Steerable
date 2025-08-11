b7dd31008efdb3e72136471442a263ac9b0762c9
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-08-11 15:10:21 -0500

    Update

--- Steerable/utils/rotate.py
+++ Steerable/utils/rotate.py
@@ -8,23 +8,23 @@ class RandomRotate(torch.utils.data.Dataset):
         
     def __getitem__(self, index):
         inputs, targets = self.dataset[index]
-        if targets.ndim == 2:
+        if inputs.ndim == 3:
             degree = torch.randint(0, 360, (1,)).item()
             inputs = rotate_image(inputs, degree, order=1)
-            targets = rotate_image(targets, degree, order=0)
-        elif targets.ndim == 3:
+            if targets.ndim == 2:
+                targets = rotate_image(targets, degree, order=0)
+        elif inputs.ndim == 4:
             degree = torch.randint(0, 360, (3,))
             inputs = rotate_image(inputs, degree, order=1)
-            targets = rotate_image(targets, degree, order=0)
+            if targets.ndim == 3:
+                targets = rotate_image(targets, degree, order=0)
         else:
             ValueError("Only 2D or 3D image data are supported.")
         return inputs, targets
-            
+ 
     def __len__(self):
         return len(self.dataset)
-    
-    
-    
+ 
 class AddGaussianNoise(torch.utils.data.Dataset):
     def __init__(self, dataset:torch.utils.data.Dataset, sd:float):
         self.dataset = dataset

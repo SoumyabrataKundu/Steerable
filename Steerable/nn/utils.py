69ba448fe11d74859b2c3994065d0fea9b2b0242
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-08-19 13:35:46 -0500

    Update

--- Steerable/nn/utils.py
+++ Steerable/nn/utils.py
@@ -13,6 +13,7 @@ import torch
 def get_interpolation_matrix(kernel_size, n_radius, n_angle, interpolation_order=1):
     assert 0 <= interpolation_order <= 5, "'interpolation_order' takes integer values between 0 and 5."
     d = len(kernel_size)
+    assert d>=2, "dimension of 'kernel_size' should be atleast 2"
     R = torch.tensor([(kernel_size[i] - 1)/2 for i in range(d)])
     A1 = torch.pi * (torch.arange(n_angle)+0.5) / n_angle
     A2 = 2 * torch.pi * torch.arange(n_angle) / n_angle
@@ -24,15 +25,13 @@ def get_interpolation_matrix(kernel_size, n_radius, n_angle, interpolation_order
                     torch.tensordot(sphere_coord[:-1], torch.ones(n_angle), dims=0),
                     torch.tensordot(sphere_coord[-1:], torch.cos(A), dims=0), 
                     torch.tensordot(sphere_coord[-1:], torch.sin(A), dims=0)])
-        
-    sphere_coord = torch.einsum('dr, da -> dra', r_values, sphere_coord.flatten(1)) + R.reshape(-1, 1,1)
-    
+    sphere_coord = (torch.einsum('dr, da -> dra', r_values, sphere_coord.flatten(1)) + R.reshape(-1, 1,1)).flatten(1)
     I = torch.zeros(n_radius * n_angle**(d-1), *kernel_size, dtype=torch.float)
     kernel = torch.cartesian_prod(*[torch.arange(0, kernel_size[i], 1) for i in range(d)])
     for i in range(len(kernel)):
         f = torch.zeros(*kernel_size)
         f[tuple(kernel[i].tolist())] = 1
-        I[(Ellipsis,) + tuple(kernel[i].tolist())] = torch.from_numpy(map_coordinates(f, sphere_coord.flatten(1), order=interpolation_order, mode='nearest'))
+        I[(Ellipsis,) + tuple(kernel[i].tolist())] = torch.from_numpy(map_coordinates(f, sphere_coord, order=interpolation_order, mode='nearest'))
 
     return I.reshape(n_radius, -1, *I.shape[-d:])
 
@@ -107,21 +106,16 @@ def get_Fint_matrix(kernel_size, n_radius, n_angle, freq_cutoff, interpolation_t
             Fint = torch.einsum('rxy, mxy-> mrxy', tau_r, Fint)
             
         elif d == 3:
-            theta = torch.acos(torch.clamp(points[2] / r, -1.0, 1.0))
+            theta = torch.nan_to_num(torch.acos(torch.clamp(points[2] / r, -1.0, 1.0)), nan=0.0)
             phi = torch.arctan2(points[1], points[0])
             Fint = []
             for l in range(freq_cutoff+1):
-                Y_lm_stack = []
-                for m in range(-l, l + 1):
-                    # Compute spherical harmonics using scipy
-                    Y_lm = torch.from_numpy(sph_harm(m, l, phi.numpy(), theta.numpy())).type(torch.cfloat)
-                    Y_lm_stack.append(torch.complex(torch.nan_to_num(Y_lm.real, nan=0.0),
-                                                    torch.nan_to_num(Y_lm.real, nan=0.0)))
-                Fint.append(torch.stack(Y_lm_stack, dim=0).reshape(-1, 1, *kernel_size)*tau_r)
+                Y_l = [torch.from_numpy(sph_harm(m, l, phi.numpy(), theta.numpy())).type(torch.cfloat) for m in range(-l, l + 1)]
+                Fint.append(torch.stack(Y_l, dim=0).reshape(-1, 1, *kernel_size)*tau_r)
         
     elif 0 <= interpolation_type and interpolation_type<=5 and type(interpolation_type) == int:
         h = torch.prod(torch.tensor([(kernel_size[i] - 1)/2 for i in range(d)]))
-        scalar = ((torch.arange(1, n_radius+1) /(n_radius+1) )**(d-1)) / (n_radius)
+        scalar = ((torch.arange(1, n_radius+1) /(n_radius+1))**(d-1)) / (n_radius)
         SHT = get_SHT_matrix(n_angle, freq_cutoff, d) # Spherical Harmonic Transform Matrix
         if d == 2:
             I = get_interpolation_matrix(kernel_size, n_radius, n_angle, interpolation_type).type(torch.cfloat) # Interpolation Matrix

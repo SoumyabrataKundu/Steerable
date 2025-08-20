d2c1d21f119fde86e5d8721bebfb6886736b7a41
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-08-20 11:49:26 -0500

    Update

--- Steerable/nn/utils.py
+++ Steerable/nn/utils.py
@@ -18,7 +18,7 @@ def get_interpolation_matrix(kernel_size, n_radius, n_angle, interpolation_order
     A1 = torch.pi * (torch.arange(n_angle)+0.5) / n_angle
     A2 = 2 * torch.pi * torch.arange(n_angle) / n_angle
     sphere_coord = torch.ones(1)
-    r_values = torch.vstack([torch.arange(1, n_radius+1)*h/(n_radius+1) for h in R])
+    r_values = torch.vstack([torch.arange(1, n_radius+1)*h/(n_radius) for h in R])
     for i in range(d-1):
         A = A1 if i<d-2 else A2
         sphere_coord = torch.vstack([
@@ -125,7 +125,7 @@ def get_Fint_matrix(kernel_size, n_radius, n_angle, freq_cutoff, interpolation_t
                 Fint.append(torch.stack(Y_l, dim=0).reshape(-1, 1, *kernel_size)*tau_r)
         
     elif 0 <= interpolation_type and interpolation_type<=5 and type(interpolation_type) == int:
-        scalar = ((torch.arange(1, n_radius+1) / ((n_radius+1) * n_angle)) ** (d-1)) # / n_radius
+        scalar = ((torch.arange(1, n_radius+1) / ((n_radius) * n_angle)) ** (d-1))  / n_radius
         SHT = get_SHT_matrix(n_angle, freq_cutoff, d) # Spherical Harmonic Transform Matrix
         if d == 2:
             I = get_interpolation_matrix(kernel_size, n_radius, n_angle, interpolation_type).type(torch.cfloat) # Interpolation Matrix
@@ -148,7 +148,7 @@ def get_CFint_matrix(kernel_size, n_radius, n_angle, freq_cutoff_in, freq_cutoff
     Fint = get_Fint_matrix(kernel_size, n_radius, n_angle, freq_cutoff, interpolation_type)
     C = get_CG_matrix(d, freq_cutoff, n_angle)
     if d == 2:
-        CFint = torch.einsum('lmn, nrxy -> lmrxy', torch.tensor(C, dtype=torch.cfloat), Fint) # / freq_cutoff
+        CFint = torch.einsum('lmn, nrxy -> lmrxy', torch.tensor(C, dtype=torch.cfloat), Fint)  / freq_cutoff
 
     elif d == 3:
         CFint = [[torch.stack([torch.einsum('lmn, nrxyz -> lrmxyz', C[l][l1][l2].type(torch.cfloat), Fint[l2]) # / (freq_cutoff + 1)

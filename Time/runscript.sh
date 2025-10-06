7fe8b1a423b62258754da0c3b42ed3c5ce4f5efe
SoumyabrataKundu <soumyabratakundu@uchicago.edu>
2025-10-06 10:08:47 -0500

    Update

--- Time/runscript.sh
+++ Time/runscript.sh
@@ -3,11 +3,7 @@ channels=(2 4 8 16 32 64)
 kernels=(2 3 4 5)
 n_radius=(2 4 6 8)
 max_m=(4 8 12 16)
-restricted=(0 1)
 conv_first=(0 1)
-
-
-
 
 for channel in "${channels[@]}"
 do
@@ -17,23 +13,20 @@ do
         do
             for k in "${max_m[@]}"
             do
-                for restrict in "${restricted[@]}"
+                for conv in "${conv_first[@]}"
                 do
-                    for conv in "${conv_first[@]}"
-                    do
-                        cp script.sh script_temp.sh
+                    cp script.sh script_temp.sh
 
-                        sed -i "s/CHANNEL/${channel}/g" script_temp.sh
-                        sed -i "s/KERNEL/${kernel}/g" script_temp.sh
-                        sed -i "s/RADIUS/${radius}/g" script_temp.sh
-                        sed -i "s/MAXM/${k}/g" script_temp.sh
-                        sed -i "s/RESTRICTED/${restrict}/g" script_temp.sh
-                        sed -i "s/CONVFIRST/${conv}/g" script_temp.sh
+                    sed -i "s/CHANNEL/${channel}/g" script_temp.sh
+                    sed -i "s/KERNEL/${kernel}/g" script_temp.sh
+                    sed -i "s/RADIUS/${radius}/g" script_temp.sh
+                    sed -i "s/MAXM/${k}/g" script_temp.sh
+                    sed -i "s/RESTRICTED/${restrict}/g" script_temp.sh
+                    sed -i "s/CONVFIRST/${conv}/g" script_temp.sh
 
-                        JOB_ID=$(sbatch script_temp.sh | awk '{print $NF}')
-                        rm script_temp.sh
+                    JOB_ID=$(sbatch script_temp.sh | awk '{print $NF}')
+                    rm script_temp.sh
 
-                    done
                 done
                 echo "Submitted Job -- Channel : ${channel}  Kernel : ${kernel}  Radius : ${radius}  Cutoff : ${k}"
 
